03f60d12ab938a0aa8c0aaa8ab48b0cd913873c6
unknown <walter.luszczyk@POLPC01468.allegrogroup.internal>
2015-09-30 09:29:22 +0200

    Initial commit - stage 0

---
+++ LoanCalculator.Core/Helpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace LoanCalculator.Core
+{
+    public static class Helpers
+    {
+        private static Random _rnd = new Random();
+
+        public static int GetRandomInt(int minValue, int maxValue)
+        {
+            return _rnd.Next(minValue, maxValue);
+        }
+
+        public static int GetRandomInt(int maxValue)
+        {
+            return GetRandomInt(0, maxValue);
+        }
+
+        public static string GetRandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * _rnd.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            return builder.ToString().ToLower();
+        }
+
+        public static string GetRandomEmail()
+        {
+            return string.Format("{0}@client.com", GetRandomString(10));
+        }
+
+        public static int GetRandomLoadId()
+        {
+            return GetRandomInt(1000000, 2000000);
+        }
+    }
+}
